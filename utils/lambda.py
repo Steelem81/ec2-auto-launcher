f8a3c868a8724e3db798a1496d935460c8d487a4
Jeff Steele <steelem81@gmail.com>
2026-02-26 21:23:11 -0800

    added dynamic bucket

--- utils/lambda.py
+++ utils/lambda.py
@@ -1,7 +1,7 @@
 import boto3
 import os
 
-def lambda_handler(event, context):
+def lambda_handler(event, context, bucket_name):
     try:
         #extract s3 details from event
         bucket = event['Records'][0]['s3']['bucket']['name']
@@ -24,7 +24,7 @@ def lambda_handler(event, context):
             file.write(uppercased_content)
 
         #upload result
-        output_bucket = 'BUCKET_NAME'
+        output_bucket = bucket_name
         output_key = f'processed/{os.path.basename(key)}'
         s3.upload_file(output_file, output_bucket, output_key)
         print(f"Uploaded to {output_bucket}/{output_key}")
